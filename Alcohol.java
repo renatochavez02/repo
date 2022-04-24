237dd634a3f9e5f1fdb3609c27af4f02112e6688
renatochavez02 <99062018+renatochavez02@users.noreply.github.com>
2022-04-23 21:41:06 -0400

    Add files via upload

---
+++ Alcohol.java
@@ -0,0 +1,51 @@
+
+public class Alcohol extends Beverage{
+	private boolean isWeekend;
+	private final double WEEKEND_FEE = 0.6;
+	
+	// Constructors
+	public Alcohol(String n, SIZE s, boolean iw) {
+		super(n, TYPE.ALCOHOL, s);
+		isWeekend = iw;
+	}
+	public String toString() {
+		String s = getBevName() + ", " + getSize();
+		if(isWeekend) {
+			s += " Weekend";
+		}
+		s += ", $" + calcPrice();
+		return s;
+	}
+	public boolean equals(Alcohol a) {
+		if(super.equals(a) && isWeekend == a.getIsWeekend()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public double calcPrice() {
+		double price = super.getBasePrice();
+		if(super.getSize() == SIZE.MEDIUM) {
+			price += super.getSizePrice();
+		}
+		else if(super.getSize() == SIZE.LARGE) {
+			price += 2 * super.getSizePrice();
+		}
+		if(isWeekend) {
+			price += WEEKEND_FEE;
+		}
+		return price;
+	}
+	public boolean getIsWeekend() {
+		return isWeekend;
+	}
+	public double getWeekendFee() {
+		return WEEKEND_FEE;
+	}
+	
+	// Mutators
+	public void setIsWeekend(boolean is) {
+		isWeekend = is;
+	}
+}
